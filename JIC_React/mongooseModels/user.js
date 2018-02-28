fb82c4d42cc577759f4ec042b7f87d2212bb02c3
kmasteller <kennethmasteller@gmail.com>
2018-02-27 20:50:07 -0700

    controllers and models added

--- JIC_React/mongooseModels/user.js
+++ JIC_React/mongooseModels/user.js
@@ -1,39 +1,57 @@
 // Require mongoose
-console.log("what up from user...");
-var mongoose = require("mongoose");
+const mongoose = require("mongoose");
 // Create the schema class using mongoose's schema method
-var Schema = mongoose.Schema;
+const Schema = mongoose.Schema;
 
-// Create the noteSchema with the schema object
-var userSchema = new Schema({
+const userSchema = new Schema({
 
-  // date is just a string
-  username: {
-    type: String,
-    required: true,
-    unique: true
+  firstname: {
+    type: STRING,
+    allowNull: false,
+    len: [1]
   },
-  name: {
-    type: String,
-    required: true
+  lastname: {
+    type: STRING,
+    allowNull: false,
+    len: [1]
   },
   email: {
     type: String,
-    required: true
+    trim: true,
+    lowercase: true,
+    unique: true,
+    required: 'Email address is required',
+    validate: [validateEmail, 'Please fill a valid email address'],
+    match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
   },
   password: {
-    type: String,
-    required: true
+    type: STRING,
+    allowNull: false,
+    len: [1]
   },
   profilePic: {
-    type: String,
-    required: false
+    type: STRING,
+    allowNull: true,
+    len: [1]
   },
   isAdmin: {
-    type: Boolean,
-    default: false
-  },
+    type: BOOLEAN,
+    allowNull: true,
+    len: [1]
+  }
 });
+
+  //there are no assosiations yet so i commented this out
+
+  // Users.associate = function (models) {
+  //   // We're saying that a Users should belong to an Author
+  //   // A Users can't be created without an Author due to the foreign key constraint
+  //   Users.belongsTo(models.<YOUR MODEL NAME>, {
+  //     foreignKey: {
+  //       allowNull: false
+  //     }
+  //   });
+  // };
 
 // Create the Note model using the noteSchema
 var User = mongoose.model("User", userSchema);
